5e14f44b4f6b3ceda143b191935f3fb3f8ced8ca
vas1levnv <vas1levnv@yandex.ru>
2022-07-17 11:18:51 +0300

    Ваиладация на email и сайт

--- app.js
+++ app.js
@@ -12,14 +12,16 @@ $(document).ready(function () {
             }
         });
     }
+
     function getInfoFile(id) {
         $.ajax({
             url: 'https://api.sitemap-generator.ru/task/' + id,
             type: "GET",
-            success: function (data) {
+            success: function (data, textStatus, xhr) {
                 let link = 'https://api.sitemap-generator.ru/sitemap/' + data.sitemap
                 $("#link").empty();
                 $("#link").append("<a href=" + link + ">скачать файл</a>")
+
             }
         });
     }
@@ -28,14 +30,34 @@ $(document).ready(function () {
         let ajaxData = {}
         ajaxData.origin = $("#origin").val();
         ajaxData.email = $("#email").val();
+        if (!ajaxData.origin) {
+            $("#post").prop('disabled', true);
+        }
+
+        function isEmail(email) {
+            let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return regex.test(String(email).toLowerCase());
+        }
+
+        if(!isEmail(ajaxData.email)){
+            alert('Введите валидный Email')
+            return false
+        }
         $.ajax({
             url: 'https://api.sitemap-generator.ru/task',
             type: "post",
             data: ajaxData,
-            success: function (data) {
+            success: function (data, textStatus, xhr) {
+                console.log(xhr.status)
                 alert('данные приняты, задача №' + data.id)
                 checkStatusId(data.id)
                 getInfoFile(data.id)
+                $("#origin").val('')
+                $("#email").val('');
+            },
+            error: function () {
+                alert('Введите валидный сайт')
+
             }
         });
 
